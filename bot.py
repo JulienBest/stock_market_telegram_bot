303b5e8cb879c201233a5d7dfb490ff2e8ba2e32
Julien <julien.best@live.de>
2020-06-10 13:11:24 +0200

    Adding help command

--- bot.py
+++ bot.py
@@ -10,3 +10,16 @@ bot = telebot.TeleBot(token=bot_token)
 @bot.message_handler(commands=['start'])
 def handle(message):
 	bot.send_message(message.chat.id, 'Welcome to the stock market bot! Type /help to see what commands are available at the moment!')
+
+
+
+@bot.message_handler(commands=['help'])
+def handle(message):
+	bot.send_message(message.chat.id, 'The following commands are available at the moment:\n\n \
+/price - Shows the current price and some historical prices \n\
+/performance - Shows the performance of a stock for different time periods \n\
+/whatdoes - Shows the general information about a company')
+
+
+def get_message_text_without_command(msg):
+	return re.sub('\/[a-z]+\s', '', msg.text)
